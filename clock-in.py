7ca8003f852eaef6c55bbe44ece1682361599f29
zsxm1998 <zsxm1998@qq.com>
2022-05-08 02:29:49 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -71,7 +71,7 @@ class ClockIn(object):
 
     def get_captcha(self):
         """Get CAPTCHA code"""
-        cookie_dict = {'eai-sess': '9k1adtbd8ti87t488f786vvtl3'}
+        cookie_dict = {'eai-sess': 'qk2bj52t1sl7obu2dfhko1fdu6'}
         self.sess.cookies = requests.cookies.cookiejar_from_dict(cookie_dict)
         resp = self.sess.get(self.CAPTCHA_URL)
         captcha = self.ocr.classification(resp.content)
